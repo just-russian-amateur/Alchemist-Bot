4f39591268edee99248747ea7e68b61749e8e8a2
just-russian-amateur <laptevalexxxander2001@gmail.com>
2024-04-04 11:07:41 +0700

    Исправление ошибок для случаев, когда замена цветов не требуется, а также компиляция исполняемого файла golang для Linux

--- fucking_game/telegram_bot_for_flasks.py
+++ fucking_game/telegram_bot_for_flasks.py
@@ -134,54 +134,112 @@ async def download_photoes(message:Message, bot: Bot):
         # Распознаем цвета и добавляем их в список с последующей сериализации в json
         config.undefined_colors = found_colors_in_flasks(image_for_search=config.image_for_load, id=message.from_user.id)
 
-        color_buttons_list = []
-        # Создание списка кнопок с цветмаи, которыми можно будет заменить неопределенные значения
-        for color in config.undefined_colors.keys():
-            for _ in range(config.undefined_colors[color]):
-                if color == 'LIGHTLIGHT':
-                    color_buttons_list.append(KeyboardButton(text='LIGHT LIGHT'))
-                elif color == 'LIGHTBLUE':
-                    color_buttons_list.append(KeyboardButton(text='LIGHT BLUE'))
-                else:
-                    color_buttons_list.append(KeyboardButton(text=color))
-        color_buttons = create_undef_buttons(color_buttons_list)
+        if len(config.undefined_colors) != 0:
+            color_buttons_list = []
+            # Создание списка кнопок с цветмаи, которыми можно будет заменить неопределенные значения
+            for color in config.undefined_colors.keys():
+                for _ in range(config.undefined_colors[color]):
+                    if color == 'LIGHTLIGHT':
+                        color_buttons_list.append(KeyboardButton(text='LIGHT LIGHT'))
+                    elif color == 'LIGHTBLUE':
+                        color_buttons_list.append(KeyboardButton(text='LIGHT BLUE'))
+                    else:
+                        color_buttons_list.append(KeyboardButton(text=color))
+            color_buttons = create_undef_buttons(color_buttons_list)
 
-        keyboard_buttons = ReplyKeyboardMarkup(
-            keyboard=color_buttons,
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            input_field_placeholder='Choose a color'
-        )
-
-        if message.text == 'Add an empty flask':
-            # Добавляем пустую колбу
-            add_empty_flask(json_name=f"./tmp/start_level_{message.from_user.id}.json")
-
-        # Подготавливаем картинку, в которой подсвечиваем неопределенные области
-        create_image_for_replace(json_name=f"./tmp/start_level_{message.from_user.id}.json", id_client=message.from_user.id)
-
-        feedback_button_set = [
-            [
-                InlineKeyboardButton(text='Feedback to me', url=f"tg://user?id={984089348}")
-            ]
-        ]
-        feedback_button = InlineKeyboardMarkup(inline_keyboard=feedback_button_set)
-
-        # Изображение, где подсвечивается первый неопределенный цвет
-        with open(f'./tmp/level_for_{message.from_user.id}.jpg', 'rb') as open_image:
-            await message.answer_photo(
-                BufferedInputFile(
-                    open_image.read(),
-                    filename='solve_flasks'
-                ),
-                caption="If I recognized some colors incorrectly, please give me feedback by clicking the button below the message (send a photo and describe the problem)",
-                reply_markup=feedback_button
+            keyboard_buttons = ReplyKeyboardMarkup(
+                keyboard=color_buttons,
+                resize_keyboard=True,
+                one_time_keyboard=True,
+                input_field_placeholder='Choose a color'
             )
-        await message.answer(
-            'Please select from the options provided the color that should be in place of the green circle',
-            reply_markup=keyboard_buttons
-        )
-        config.start_replace = True
+
+            if message.text == 'Add an empty flask':
+                # Добавляем пустую колбу
+                add_empty_flask(json_name=f"./tmp/start_level_{message.from_user.id}.json")
+
+            # Подготавливаем картинку, в которой подсвечиваем неопределенные области
+            create_image_for_replace(json_name=f"./tmp/start_level_{message.from_user.id}.json", id_client=message.from_user.id)
+
+            feedback_button_set = [
+                [
+                    InlineKeyboardButton(text='Feedback to me', url=f"tg://user?id={984089348}")
+                ]
+            ]
+            feedback_button = InlineKeyboardMarkup(inline_keyboard=feedback_button_set)
+
+            # Изображение, где подсвечивается первый неопределенный цвет
+            with open(f'./tmp/level_for_{message.from_user.id}.jpg', 'rb') as open_image:
+                await message.answer_photo(
+                    BufferedInputFile(
+                        open_image.read(),
+                        filename='solve_flasks'
+                    ),
+                    caption="If I recognized some colors incorrectly, please give me feedback by clicking the button below the message (send a photo and describe the problem)",
+                    reply_markup=feedback_button
+                )
+            await message.answer(
+                'Please select from the options provided the color that should be in place of the green circle',
+                reply_markup=keyboard_buttons
+            )
+            config.start_replace = True
+        else:
+            # Формируем итоговый json
+            create_image_for_replace(json_name=f"./tmp/start_level_{message.from_user.id}.json", id_client=message.from_user.id)
+            # Итоговое изображение
+            with open(f'./tmp/level_for_{message.from_user.id}.jpg', 'rb') as open_image:
+                await message.answer_photo(
+                    BufferedInputFile(
+                        open_image.read(),
+                        filename='solve_flasks'
+                    ),
+                    caption="I'll look for a solution from this position. Wait, this may take a while",
+                    reply_markup=ReplyKeyboardRemove()
+                )
+
+            config.start_replace = False
+            flasks_solver(input_file=f"./tmp/start_level_{message.from_user.id}.json", output_file=f"./tmp/result_level_{message.from_user.id}.txt")
+
+            download_again = [
+                [
+                    KeyboardButton(text='Upload new image')
+                ]
+            ]
+            download_buttons = ReplyKeyboardMarkup(
+                keyboard=download_again,
+                resize_keyboard=True,
+                one_time_keyboard=True
+            )
+
+            errors = [
+                [
+                    KeyboardButton(text='Reload image'),
+                    KeyboardButton(text='Add an empty flask'),
+                    KeyboardButton(text='Upload new image')
+                ]
+            ]
+            error_buttons = ReplyKeyboardMarkup(
+                keyboard=errors,
+                resize_keyboard=True,
+                one_time_keyboard=True
+            )
+
+            if os.stat(f"./tmp/result_level_{message.from_user.id}.txt").st_size == 0:
+                await message.answer(
+                    f'Unfortunately, I was unable to find a solution for this arrangement.\nIf you want to change the order of undefined colors, click "Reload image".\nIf you know all the colors, but the solution still hasn’t been found, then I can add another empty flask, to do this, click “Add an empty flask”\nOr you can upload a new image, to do this, click "Upload new image"',
+                    reply_markup=error_buttons
+                )
+                config.reload_image = True
+            else:
+                with open(f"./tmp/result_level_{message.from_user.id}.txt", "r") as result:
+                    await message.answer(
+                        f'I found a solution for you!\nPlease note that the flasks are numbered starting from 0, not 1!\n{result.read()}\nLet me know if you want a solution for another screenshot :)',
+                        reply_markup=download_buttons
+                    )
+            # Удаление временных файлов
+            os.remove(f"./tmp/result_level_{message.from_user.id}.txt")
+            os.remove(f"./tmp/start_level_{message.from_user.id}.json")
+            os.remove(f"./tmp/level_for_{message.from_user.id}.jpg")
     else:
         await message.answer('Click on the button below please :)')
     

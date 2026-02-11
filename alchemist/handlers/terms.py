781ebd2b427705d5cd6fae274e492595010cb346
just_amateur <55921215+just-russian-amateur@users.noreply.github.com>
2026-02-11 14:32:50 +0700

    Изменен адрес ресурса с политиками для пользователей

--- alchemist/handlers/terms.py
+++ alchemist/handlers/terms.py
@@ -16,7 +16,7 @@ async def terms(message: Message):
     logger.log_info(f'Пользователем {message.from_user.id} был вызван показ пользовательского соглашения')
     await message.delete()
     await message.answer(
-        'By starting to work with me you agree to the <a href="https://alexxxander-laptev.github.io/terms_for_alchemist_bot/Terms_of_Service.html">User Agreement</a> and <a href="https://alexxxander-laptev.github.io/terms_for_alchemist_bot/Privacy_Policy.html">Privacy Policy</a>\n\nYou can ask my developer any questions you have regarding the operation of the bot or the payment system through the feedback system using the command /support🙂\n\nAfter clicking the button below you can continue from where you left off',
+        'By starting to work with me you agree to the <a href="https://just-russian-amateur.github.io/terms_for_alchemist_bot/Terms_of_Service.html">User Agreement</a> and <a href="https://alexxxander-laptev.github.io/terms_for_alchemist_bot/Privacy_Policy.html">Privacy Policy</a>\n\nYou can ask my developer any questions you have regarding the operation of the bot or the payment system through the feedback system using the command /support🙂\n\nAfter clicking the button below you can continue from where you left off',
         parse_mode='HTML',
         reply_markup=ok()
     )
@@ -30,7 +30,7 @@ async def terms(callback: CallbackQuery):
     '''Функция для показа пользовательского соглашения'''
     logger.log_info(f'Пользователем {callback.from_user.id} был вызван показ пользовательского соглашения')
     await callback.message.edit_text(
-        'By starting to work with me you agree to the <a href="https://alexxxander-laptev.github.io/terms_for_alchemist_bot/Terms_of_Service.html">User Agreement</a> and <a href="https://alexxxander-laptev.github.io/terms_for_alchemist_bot/Privacy_Policy.html">Privacy Policy</a>\n\nYou can ask my developer any questions you have regarding the operation of the bot or the payment system through the feedback system using the command /support🙂',
+        'By starting to work with me you agree to the <a href="https://just-russian-amateur.github.io/terms_for_alchemist_bot/Terms_of_Service.html">User Agreement</a> and <a href="https://alexxxander-laptev.github.io/terms_for_alchemist_bot/Privacy_Policy.html">Privacy Policy</a>\n\nYou can ask my developer any questions you have regarding the operation of the bot or the payment system through the feedback system using the command /support🙂',
         parse_mode='HTML',
         reply_markup=ok()
     )
